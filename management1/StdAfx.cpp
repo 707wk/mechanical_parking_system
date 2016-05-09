fb9094921d2527f18441af687ed5caca26ce4a15
707wk <gtsoft_wk@foxmail.com>
2016-05-09 16:13:06 +0800

    Signed-off-by: 707wk <gtsoft_wk@foxmail.com>

--- management1/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	car_module_dlg.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
